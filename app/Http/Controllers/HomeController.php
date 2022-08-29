492ef9131f1713bfb374a244be06fca85ed8a43c
pablo <pablo_szu@hotmail.com>
2022-08-29 18:56:05 -0300

    creo proyecto y pruebo api

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $location = 'New York';
+
+        $queryString = http_build_query([
+          'access_key' => '92148faafe9093cec9c22987653f9910',
+          'query' => $location,
+        ]);
+        
+        $ch = curl_init(sprintf('%s?%s', 'http://api.weatherstack.com/current', $queryString));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+        $json = curl_exec($ch);
+        curl_close($ch);
+        
+        $api_result = json_decode($json, true);
+
+        return view('home', compact('api_result'));
+    }
+}
